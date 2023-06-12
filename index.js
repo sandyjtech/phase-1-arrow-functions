5edcff430cc50e5889b5904b4daceea2a57be645
Sandra G <sandyjtech@gmail.com>
2023-06-12 07:34:09 -0700

    added calculator arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (a, b) => {
+  return a / b;
+};
+divide(2000, 100);
+const square = (number) => {
+  return number * number;
+};
+square(2);
+
+const add = (a, b) => {
+  return a + b;
+};
+add(3, 4);
